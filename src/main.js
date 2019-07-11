538f4961c2873f8ceb709ba76ec763e7d7b244bc
wangzhenglian <1769032667@qq.com>
2019-07-11 16:34:20 +0800

    import the axios plugin and configuring it to a global api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import './plugins/axios'
+// import './plugins/axios'
 import App from './App.vue'
 import router from '../src/router/router'
  //引入elemen-ui插件
@@ -7,6 +7,9 @@ import Element from 'element-ui'
 
 import i18n from './lang' //引入多语言切换功能插件
 
+
+//引入axios插件
+import axios from 'axios';
 
 import store from './store'
 import './styles/element-variables.scss' //element-ui css
@@ -30,11 +33,12 @@ Vue.use(Element, {
   //size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
+// Vue.use(myCharts);
 // 引用axios，并设置基础URL为后端服务api地址
 // var axios = require('axios')
 // axios.defaults.baseURL = 'https://localhost:8080/api'
 // 将API方法绑定到全局
-// Vue.prototype.$axios = axios
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
